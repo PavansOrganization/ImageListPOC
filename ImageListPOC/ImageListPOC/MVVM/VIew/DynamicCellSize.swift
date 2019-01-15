025d8d768672ca3bf9a9dd56805b317f84537056
Pavan Shisode <pavansisode@gmail.com>
2019-01-15 14:58:24 +0530

    Build 1.0 - Added string extension file to calculate height.

--- ImageListPOC/ImageListPOC/MVVM/VIew/DynamicCellSize.swift
+++ ImageListPOC/ImageListPOC/MVVM/VIew/DynamicCellSize.swift
@@ -178,12 +178,3 @@ class CellSizeProvider {
         return CGSize(width: width, height: height + extraHeight) //150
     }
 }
-
-extension String {
-    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
-        
-        return ceil(boundingBox.height)
-    }
-}
