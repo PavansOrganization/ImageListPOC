92a20c5c515a97db7e5ae72c1f80b84a3c829fc3
Pavan Shisode <pavansisode@gmail.com>
2019-01-15 10:25:54 +0530

    Build 1.0 - Project Structure created, Storyboard removed.

--- ImageListPOC/ImageListPOC/MVVM/VIew/ImageListViewController.swift
+++ ImageListPOC/ImageListPOC/MVVM/VIew/ImageListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImageListViewController.swift
 //  ImageListPOC
 //
 //  Created by test on 15/01/19.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ImageListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-
 }
-
