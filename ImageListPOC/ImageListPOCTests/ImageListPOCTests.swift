50f63eaa676cb1103778f49e1ae51cab14dd739e
Pavan Shisode <pavansisode@gmail.com>
2019-01-21 13:41:04 +0530

    Build 1.0 - Added more test cases to test UI component configured/added correctly.

--- ImageListPOC/ImageListPOCTests/ImageListPOCTests.swift
+++ ImageListPOC/ImageListPOCTests/ImageListPOCTests.swift
@@ -10,25 +10,51 @@ import XCTest
 @testable import ImageListPOC
 
 class ImageListPOCTests: XCTestCase {
-
+    
+    var imageListViewController = ImageListViewController()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testViewController() {
+        XCTAssertNotNil(imageListViewController, "ViewController must be there, it should not be a nil")
     }
+    
+    func testCollectionView() {
+        // Checking for views added programatically on view controllers.
+        
+        let views = imageListViewController.view.subviews
+        var collectionView: UICollectionView?
+        
+        for view in views {
+            if view is UICollectionView {
+                collectionView = view as? UICollectionView
+                break
+            }
+        }
+        
+        XCTAssertNotNil(collectionView, "collectionView should not be a nil")
+    }
+    
+    func testActivityIndicator() {
+        //Checking for activity indicator
+        
+        imageListViewController.loadViewIfNeeded()
+   
+        XCTAssertEqual(imageListViewController.activityView.center, imageListViewController.view.center, "activityView should be center to superview")
+        XCTAssertEqual(imageListViewController.activityView.color, UIColor.black, "activityView should be black in color")
+        
+        XCTAssertTrue(imageListViewController.activityView.hidesWhenStopped, "activityView should be gets stopped, when hides.")
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        if Network.connectedToNetwork() { // Checking for activity Indicator is animating or not.
+            XCTAssertTrue(imageListViewController.activityView.isAnimating, "activityIndicatorView should be animating")
+        } else {
+            XCTAssertFalse(imageListViewController.activityView.isAnimating, "activityIndicatorView should not be animating")
         }
     }
-
 }
