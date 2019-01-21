382be7215e68301594aec341c228104a309bc05c
Pavan Shisode <pavansisode@gmail.com>
2019-01-21 16:05:13 +0530

    Build 1.0 - Added test cases for model Facts & Rows.

---
+++ ImageListPOC/ImageListPOCTests/ModelDataTest.swift
@@ -0,0 +1,59 @@
+//
+//  ModelDataTest.swift
+//  ImageListPOCTests
+//
+//  Created by test on 21/01/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import XCTest
+@testable import ImageListPOC
+
+class ModelDataTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+ 
+    func testFactData() {
+        let jsonData = """
+                            {
+                             "title":"About Canada",
+                             "rows":[
+                                    {
+                                    "title":"Fact title",
+                                    "description":"Fact description",
+                                    "imageHref":"path"
+                                    }
+                                    ]
+                            }
+                            """.data(using: .utf8)!
+        
+        let json = try! JSONDecoder().decode(Facts.self, from: jsonData)
+        
+        XCTAssertEqual(json.title, "About Canada")
+        XCTAssertNotNil(json.rows, "Rows array should not be nil")
+        XCTAssertTrue(json.rows?.count == 1, "rows array should have one object")
+    }
+    
+    func testRowData() {
+        let jsonData = """
+                        {
+                        "title":"Row title",
+                        "description":"Row description",
+                        "imageHref":"path"
+                        }
+                        """.data(using: .utf8)!
+        
+        let json = try! JSONDecoder().decode(Rows.self, from: jsonData)
+        
+        XCTAssertEqual(json.title, "Row title")
+        XCTAssertEqual(json.description, "Row description")
+        XCTAssertEqual(json.imageHref, "path")
+    }
+    
+}
